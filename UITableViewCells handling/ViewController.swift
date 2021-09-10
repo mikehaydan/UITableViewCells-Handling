08b473ec1497950c5975e342351afacf83380510
Mike Haydan <mykhailo.haydan@icabbi.com>
2021-09-10 18:22:49 +0300

    Initial Commit

---
+++ UITableViewCells handling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewCells handling
+//
+//  Created by Mykhailo Haidan on 10.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
